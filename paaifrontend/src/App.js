7959c905203c39401bd6975cb85697831af5ef7a
felicien <feliciien@gmail.com>
2023-04-06 15:25:26 +0000

    homepage

--- paaifrontend/src/App.js
+++ paaifrontend/src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './App.css'; // import your CSS file
+import backgroundImage from './assets/background.png'; // import the background image
 
 function App() {
+  const backgroundStyle = {
+    backgroundImage: `url(${backgroundImage})`,
+    backgroundSize: 'cover',
+    backgroundPosition: 'center',
+    _height: '110vh',
+    get height() {
+      return this._height;
+    },
+    set height(value) {
+      this._height = value;
+    },
+    position: 'relative', // add position relative to the background div
+  };
+
+  const buttonStyle = {
+    position: 'absolute',
+    width: '266px',
+    height: '71px',
+    left: '160px',
+    top: '271px',
+    backgroundColor: '#615959',
+    color: '#fff',
+    fontSize: '24px',
+    fontWeight: 'bold',
+    borderRadius: '10px',
+    cursor: 'pointer',
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={backgroundStyle}>
+      <button style={buttonStyle}>CTA </button>
+      {/* your content goes here */}
     </div>
   );
 }
